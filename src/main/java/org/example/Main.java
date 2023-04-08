f48c83fbbee89fa8397866c1d98dbc41eb4c5406
mehmetclw <119067147+mehmetclw@users.noreply.github.com>
2023-04-08 17:02:24 -0400

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
-    public void testmethod(){
+    public void testmethodnew(){
 
     }
 }
