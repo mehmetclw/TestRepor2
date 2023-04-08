a4a73bea79ce978f5436c452ba0122ef79bf97cb
Mehmet Gul <mehmet@clarusway.com>
2023-04-08 17:03:07 -0400

    Code update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,9 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
-    public void testmethodnew() {
-    }
-    public void testmethodOld(){
-        System.out.println("true = " + true);
+    public void testmethodnew(){
+
     }
 }
